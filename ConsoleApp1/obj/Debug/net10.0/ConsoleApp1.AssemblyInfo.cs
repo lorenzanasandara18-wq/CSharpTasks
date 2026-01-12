4f5536ec120bc571b9b16a385c5bba20a1105a54
lorenzanasandara18-wq <lorenzanasandara18@gmail.com>
2026-01-12 11:53:59 +0000

    Task2

--- ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb3698b02ad8bffdfb47f62ddda250698f8a2b2b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ef283e0260bf9ba060c0cfe6ed145637f79bd3c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
